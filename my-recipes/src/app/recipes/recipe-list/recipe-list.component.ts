e5132603559ac45eca000f30c8cc0d0b8ede8c46
Mihai-Canea <51034102+Mihai-Canea@users.noreply.github.com>
2020-04-02 11:59:43 +0200

    Aggiunte recipe-list

--- my-recipes/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-recipes/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', "This is simply a test", "https://www.cucchiaio.it/content/cucchiaio/it/ricette/2016/03/pancake-con-il-bimby/jcr:content/imagePreview.img10.jpg/1458722940545.jpg"),
+    new Recipe('','',''),
+    new Recipe('','',''),
+    new Recipe('','',''),
+    new Recipe('','',''),
+    new Recipe('','',''),
+    new Recipe('','',''),
+    new Recipe('','',''),
+    new Recipe('','',''),
+  ]
 
   constructor() { }
 
